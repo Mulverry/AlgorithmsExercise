b991cca6e762208026cd826fec2e2ca6aad9df73
Mulverry <125805516+Mulverry@users.noreply.github.com>
2023-04-26 00:27:51 +0900

    [Gold III] Title: 아침 산책, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,33 +1,31 @@
 import sys
-sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 n = int(input())
-a = '0' + input().rstrip()  # rstrip 안 하면 \n이 남아버림. 앞에 0붙인 이유는 a[i]가 i번째임을 바로 알기 위해서
-
-graph = [[] for _ in range(n+1)] 
-for _ in range(1, n):
-    x, y = map(int, input().split())
-    graph[x].append(y) # 양방향. 4에 시작해서 1에 끝날 수도 있음.
-    graph[y].append(x)
-
-visited = [False]*(n+1)
+status = '0'+input().rstrip()
+graph = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 cnt = 0
 
-def dfs(start):
+def dfs(x):
     global cnt
-    for i in graph[start]: # start 노드와 인접한 노드들 중에서
-        if visited[i] == False:
-            if a[i] == "1":
+    for i in graph[x]:
+        if not visited[i]:
+            if status[i] == '1':
                 cnt += 1
-                visited[i] = True
-                dfs(i)
+                visited[i] = 1
             else:
-                visited[i] = True
+                visited[i] = 1
                 dfs(i)
-                
-                  
+                visited[i] = 0
+            
 for i in range(1, n+1):
-    dfs(i)
+    visited = [0]*(n+1)
+    visited[i] = 1
+    if status[i] == '1':
+        dfs(i)
 
 print(cnt)
