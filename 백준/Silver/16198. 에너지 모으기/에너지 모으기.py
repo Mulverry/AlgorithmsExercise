2211c421bc77b863281702297ec0f2ca421cb1e1
Mulverry <125805516+Mulverry@users.noreply.github.com>
2023-05-12 21:48:47 +0900

    [Silver I] Title: 에너지 모으기, Time: 104 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+balls = list(map(int, input().split()))
+ans = 0
+
+def dfs(balls, energy):
+    global ans
+    if len(balls) == 2: # 최대에너지 구하는 조건
+        ans = max(energy, ans)
+        return
+            
+    for i in range(1, len(balls)-1):
+        new_balls = balls[:i] + balls[i+1:] # 선택한 구슬 제거
+        dfs(new_balls, energy + (balls[i-1] * balls[i+1]))
+        
+dfs(balls, 0)
+
+print(ans)
