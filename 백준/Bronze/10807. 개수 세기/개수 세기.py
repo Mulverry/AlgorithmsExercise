72b9c2956a8ef5b2f58898da4e752e674f302627
Mulverry <125805516+Mulverry@users.noreply.github.com>
2023-05-22 14:51:04 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num_list = list(map(int, input().split()))
+v = int(input())
+cnt = 0
+
+for i in num_list:
+    if i == v:
+        cnt += 1
+        
+print(cnt)
