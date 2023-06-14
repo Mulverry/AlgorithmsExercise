ed130968e2ff183c25526644938d44530df18d49
Mulverry <125805516+Mulverry@users.noreply.github.com>
2023-06-14 10:06:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,11 +1,17 @@
 from collections import Counter
 
 def solution(num_list):
-    odd, even = [], []
+    # odd, even = [], []
+    # for i in num_list:
+    #     if i % 2 == 1:
+    #         odd.append(i)
+    #     else:
+    #         even.append(i)
+    # answer = [len(even), len(odd)]
+    answer = [0, 0]
     for i in num_list:
-        if i % 2 == 1:
-            odd.append(i)
+        if i % 2 == 0:
+            answer[0] += 1
         else:
-            even.append(i)
-    answer = [len(even), len(odd)]
+            answer[1] += 1
     return answer
