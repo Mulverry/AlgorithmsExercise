b38290012c740d9a706542c6769e7b9fd4aee37e
Mulverry <125805516+Mulverry@users.noreply.github.com>
2023-10-17 21:37:33 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,13 @@
+function solution(rsp) {
+    var answer = [];
+    for (i=0; i<rsp.length; i++){
+        if (rsp[i] == '2'){
+            answer.push('0')
+        } else if (rsp[i] == '0'){
+            answer.push('5')
+        } else{
+            answer.push('2')
+        }
+    }
+    return answer.join("");
+}
